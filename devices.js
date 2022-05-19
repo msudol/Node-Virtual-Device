3de783038134a348bbbac346ef7acace8acf5741
Matt Sudol <msudol@users.noreply.github.com>
2022-05-19 12:12:46 -0400

    Update devices.js

--- devices.js
+++ devices.js
@@ -5,12 +5,39 @@
 let devices = [
     {
         name: "Test 1",
-        port: 39990
+		driver: "JSON",
+        port: 39990,
+		interval: 10,
+		data: [
+			{
+				name: "Exit Temp",
+				dataType: "range",
+				range: [140, 160],
+				chance: 5,
+				xrange: [-10, 10]
+			},
+			{
+				name: "Edge Spacing",
+				dataType: "range",
+				range: [-20, 20],
+				chance: 5,
+				xrange: [-10, 10]
+			}
+		]	
     },
     {
         name: "Test 2",
-		port: 39991
+		driver: "JSON",
+		port: 39991,
+		interval: 10,
+		data: [
+			{
+				name: "Example Bool",
+				dataType: "boolean",
+			}
+		]	
     }
 ];
 
 module.exports = devices;
+
