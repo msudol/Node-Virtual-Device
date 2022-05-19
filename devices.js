a9d40cc01b1fa5f441d2f8f9475f811fbe0288c2
Matt Sudol <msudol@users.noreply.github.com>
2022-05-19 16:14:35 -0400

    Update devices.js

--- devices.js
+++ devices.js
@@ -5,23 +5,23 @@
 let devices = [
     {
         name: "Test 1",
-		driver: "JSON",
+		driver: "JSON",  // socket will send results in json format
         port: 39990,
-		interval: 10,
+		interval: 10,  // time in seconds data will write to the socket
 		data: [
 			{
 				name: "Exit Temp",
 				dataType: "range",
 				range: [140, 160],
-				chance: 5,
-				xrange: [-10, 10]
+				chance: 50,  // percentage chance to return a value beyond the range, set high to occur often
+				xrange: [120, 180] // extended range if chance returns true
 			},
 			{
 				name: "Edge Spacing",
 				dataType: "range",
 				range: [-20, 20],
-				chance: 5,
-				xrange: [-10, 10]
+				chance: 50,
+				xrange: [-40, 40]
 			}
 		]	
     },
@@ -34,10 +34,55 @@ let devices = [
 			{
 				name: "Example Bool",
 				dataType: "boolean",
+			},
+			{
+				name: "Example Function",
+				dataType: "function", // pass a simple function to the device data generator
+				func: function() { 
+					var counter = memory.test2.lastVal++;  // use the memory object below to store a value and increment it
+					if (counter > 50) {
+						memory.test2.lastVal = 0;
+					}
+					return counter;
+				}
 			}
 		]	
-    }
+    },
+    {
+        name: "Test 3",
+		driver: "CSV",
+		port: 39992,
+		interval: 10,
+		data: [
+			{
+				name: "Example Bool",
+				dataType: "boolean",
+			},
+			{
+				name: "Example Function",
+				dataType: "function", // pass a simple function to the device data generator
+				func: function() { 
+					var counter = memory.test3.lastVal++;  // use the memory object below to store a value and increment it
+					if (counter > 50) {
+						memory.test3.lastVal = 0;
+					}
+					return counter;
+				}
+			}
+		]	
+    }	
 ];
+
+// an object space to store some things, useful to reference for storing values or advanced operations
+let memory = {
+	test2: {
+		lastVal: 0
+	},
+	test3: {
+		lastVal: 0
+	}
+};
+
 
 module.exports = devices;
 
